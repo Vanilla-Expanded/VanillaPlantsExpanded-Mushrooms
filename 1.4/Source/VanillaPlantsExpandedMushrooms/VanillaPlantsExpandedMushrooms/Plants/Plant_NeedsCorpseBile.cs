327e041718ad228885f37098e3d1341eaffd51bb
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-07-07 11:14:13 +0200

    v0.1

---
+++ 1.4/Source/VanillaPlantsExpandedMushrooms/VanillaPlantsExpandedMushrooms/Plants/Plant_NeedsCorpseBile.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using UnityEngine;
+
+using Verse;
+using Verse.Noise;
+
+namespace VanillaPlantsExpandedMushrooms
+{
+    public class Plant_NeedsCorpseBile : Plant
+    {
+
+        public override float GrowthRate
+        {
+            get
+            {
+              
+                if (base.Spawned && !PlantUtility.GrowthSeasonNow(base.Position, base.Map, false))
+                {
+                    return 0f;
+                }
+                return this.GrowthRateFactor_Fertility * this.GrowthRateFactor_Temperature * this.GrowthRateFactor_Light * this.GrowthRateFactor_CorpseBile;
+            }
+        }
+
+        public float GrowthRateFactor_CorpseBile
+        {
+            get
+            {
+                bool flag = false;
+                List<Thing> thingList = this.Position.GetThingList(this.Map);
+                foreach(Thing thing in thingList)
+                {
+                    if(thing.def == ThingDefOf.Filth_CorpseBile)
+                    {
+                        flag = true;
+                    }
+                }
+
+                if (flag)
+                {
+                    return 1f;
+                }
+                else return 0.05f;
+            }
+        }
+    }
+}
