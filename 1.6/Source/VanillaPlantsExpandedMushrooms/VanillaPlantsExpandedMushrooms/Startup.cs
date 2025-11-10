7f7cb00f6bd79c6c4c7a6e5c137ce1f8ecd3d90a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-11-10 10:02:59 +0100

    v3.03

---
+++ 1.6/Source/VanillaPlantsExpandedMushrooms/VanillaPlantsExpandedMushrooms/Startup.cs
@@ -0,0 +1,55 @@
+﻿using RimWorld;
+using System.Linq;
+using System.Collections.Generic;
+using Verse;
+
+namespace VanillaPlantsExpandedMushrooms
+{
+    [StaticConstructorOnStartup]
+    public static class Startup
+    {
+        static Startup()
+        {
+            
+
+            var beds = DefDatabase<ThingDef>.AllDefsListForReading
+                .Where(d => d.IsBed)
+                .ToList();
+
+           
+            foreach (var bed in beds)
+            {
+                AddAffectedByFacilityComp(bed);
+            }
+
+            
+        }
+
+        private static void AddAffectedByFacilityComp(ThingDef def)
+        {
+            if (def.comps == null)
+            {
+                def.comps = new List<CompProperties>();
+            }
+
+            var existingComp = def.GetCompProperties<CompProperties_AffectedByFacilities>();
+            if (existingComp != null)
+            {
+                existingComp.linkableFacilities.Add(InternalDefOf.VPE_Plant_Lullabloom);
+               
+            }
+            else
+            {
+                var comp = new CompProperties_AffectedByFacilities
+                {
+                    linkableFacilities = new List<ThingDef> { InternalDefOf.VPE_Plant_Lullabloom }
+                };
+                
+                def.comps.Add(comp);
+                
+            }
+        }
+
+       
+    }
+}
